03a36cf72c7dae28222da556c3720b7c563e32ee
黄宇 <hyuishine@gmail.com>
2020-12-13 22:39:37 +0800

    修改布局兼容，改为手写断点

--- src/view/layout/index.vue
+++ src/view/layout/index.vue
@@ -2,29 +2,39 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2020-12-09 22:44:42
  * @LastEditors: 黄宇/hyuishine
- * @LastEditTime: 2020-12-09 23:14:26
+ * @LastEditTime: 2020-12-13 22:08:44
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
  * @youWant: add you want
 -->
 <template>
-  <v-app id="inspire">
-    <v-container fluid>
-      <v-main>
-        <v-flex>
-          <v-layout flex
-                    justify-space-between
-                    row>
-            <v-row align="center"
-                   justify="center">
-              <keep-alive>
-                <router-view></router-view>
-              </keep-alive>
-            </v-row>
-          </v-layout>
-        </v-flex>
-      </v-main>
-    </v-container>
-  </v-app>
+  <div>
+    <!-- 主内容 打开菜单时 缩小 通过绑定侧栏菜单状态 拼接字符串匹配样式-->
+    <v-app id="inspire">
+      <v-container fluid>
+        <v-main>
+          <v-flex id="viewBox">
+            <v-layout flex
+                      justify-space-between
+                      row
+                      wrap>
+              <v-row align="center"
+                     justify="center">
+                <v-col :lg="8"
+                       :md="10"
+                       :sm="12"
+                       :xs="12">
+                  <!-- 真的内容，敢于嵌套多层娃 -->
+                  <keep-alive>
+                    <router-view></router-view>
+                  </keep-alive>
+                </v-col>
+              </v-row>
+            </v-layout>
+          </v-flex>
+        </v-main>
+      </v-container>
+    </v-app>
+  </div>
 </template>
