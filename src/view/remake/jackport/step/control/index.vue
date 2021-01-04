73d164537e4b639ed4fe1e19786cefbfadeadf92
黄宇 <hyuishine@gmail.com>
2021-01-04 20:55:15 +0800

    切回 生成代码分支 抄一抄自己写的数据表

--- src/view/remake/jackport/step/control/index.vue
+++ src/view/remake/jackport/step/control/index.vue
@@ -2,7 +2,7 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2020-12-26 12:14:32
  * @LastEditors: 黄宇/hyuishine
- * @LastEditTime: 2020-12-26 17:45:03
+ * @LastEditTime: 2021-01-04 20:49:53
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
@@ -12,18 +12,6 @@
   <v-row justify='space-between'>
     <v-col>
       <import-excel></import-excel>
-    </v-col>
-    <v-col>
-      <v-btn color="success"
-             text>
-        手动添加数据
-      </v-btn>
-    </v-col>
-    <v-col>
-      <v-btn color="warning"
-             text>
-        编辑数据
-      </v-btn>
     </v-col>
   </v-row>
 </template>
