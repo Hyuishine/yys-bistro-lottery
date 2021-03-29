f36c2fb37052bf57f55c721dedeb036ddb87d6e5
黄宇 <huangyu@3xdata.cn>
2021-03-29 14:27:39 +0800

    添加指针，更新变量获取公式，编写滚动实现方法

--- src/view/rouletteTest/index.vue
+++ src/view/rouletteTest/index.vue
@@ -2,7 +2,7 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2021-03-25 17:11:38
  * @LastEditors: 黄宇/hyuishine
- * @LastEditTime: 2021-03-26 17:22:04
+ * @LastEditTime: 2021-03-29 14:25:20
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
@@ -24,10 +24,11 @@
 顶角 = 360 / 三角形数量  
 间隔 = （360 % 三角形数量）/ （三角形数量）
 底角 = (180 - 顶角) / 2
-底边 = 顶角 * 4
+底边 = 顶角 * 4(因该是巧合)
 
 能容纳多少个三角形 : 360 / ( 单个三角形顶角 + 间隔角度)
-旋转角度：(顶角 + 间隔角度) * index
+每格旋转角度：i * (360 / 顶角)
+轮盘每次滚动角度：360 / 顶角
 
 底角计算公式：底角=atan(高/(底边/2))。    Math.atan(高/(底边/2))
 顶角计算公式：顶角=90度-底角=2*atan((底边/2)/高)。 90 - 2 * Math.atan(高/(底边/2))
@@ -44,10 +45,16 @@
         开始
       </v-btn>
     </div>
-    <div id="wheel_container">
-      <span v-for="i in 360"
+    <!-- 奖池外框 + 指针 -->
+    <div id="wheel_container"
+         :style="'transform:rotate(' + scrollNums * (360 / items.length) + 'deg)'">
+      <!-- 奖池 -->
+      <span v-for="(item, i) in items"
             :key="i"
-            :style="'transform:rotate('+ i * (360 / items) + 'deg);--borderWdith:'+ (360 / items) * 2 +'px;'"></span>
+            :style="'transform:rotate('+ (i - 1) * (360 / items.length) + 'deg);--borderWdith:'+ ( 360 /items.length) * 2 +'px;'">
+        <!-- 奖池上显示的文字 -->
+        <i>{{ item }}</i>
+      </span>
     </div>
   </v-card>
 </template>
@@ -56,26 +63,18 @@ export default {
   name: 'components_randomwheel',
   data () {
     return {
+      // 滚动单位数
+      scrollNums: 0,
       // 等腰三角形数量
-      items: 10,
+      items: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
     }
   },
   methods: {
     start () {
-      document.getElementById('wheel_container')
     },
-    randomColor () {
-      let r = Math.floor(Math.random() * 255);
-      let g = Math.floor(Math.random() * 255);
-      let b = Math.floor(Math.random() * 255);
-      return 'rgba(' + r + ',' + g + ',' + b + ',0.8)'
-    }
   },
   mounted () {
-    let dom = document.getElementById("myId")
-    for (let i = 0; i < this.items.length; i++) {
-      window.getComputedStyle(dom[i], ":before")
-    }
+
   }
 }
 </script>
@@ -85,24 +84,30 @@ export default {
   width: 500px;
   height: 500px;
   margin: 200px auto;
+  // 外圈和内圈
   .wheel_out {
     z-index: 2;
     width: 100%;
     height: 100%;
     background: transparent;
     border: 40px solid white;
-    &,
-    .wheel_in {
-      position: relative;
-      border-radius: 50% !important;
-      transform: translate(-50%, -50%);
-      top: 50%;
-      left: 50%;
-    }
-    .wheel_in {
-      width: 30%;
-      height: 30%;
-      background: white;
+    position: relative;
+    border-radius: 50% !important;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    // 指针
+    &::before {
+      content: "";
+      margin: 0px calc(50% - 15px);
+      display: block;
+      width: 0px;
+      border: 15px solid transparent;
+      border-top-style: solid;
+      border-top-width: 40px;
+      position: absolute;
+      background: transparent;
+      border-top-color: white;
     }
   }
 }
@@ -113,6 +118,7 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
+  // 随机项
   span {
     position: absolute;
     border-radius: 50% !important;
@@ -142,6 +148,18 @@ export default {
     }
     &:nth-child(2n)::before {
       border-top-color: black;
+    }
+    // 文字
+    i {
+      display: block;
+      font-style: normal;
+      font-weight: bold;
+      color: white;
+      position: absolute;
+      left: calc(50% - 4px);
+      top: 12%;
+      width: 14px;
+      font-size: 10px;
     }
   }
 }
