0e158b7c15e2260bc1157c0650c72395a3af121e
黄宇 <hyuishine@gmail.com>
2021-03-29 17:47:38 +0800

    编写开始和结束事件

--- src/view/wheelRandom/index.vue
+++ src/view/wheelRandom/index.vue
@@ -2,7 +2,7 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2021-03-25 17:11:38
  * @LastEditors: 黄宇/hyuishine
- * @LastEditTime: 2021-03-29 15:33:31
+ * @LastEditTime: 2021-03-29 17:46:37
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
@@ -34,46 +34,73 @@
 顶角计算公式：顶角=90度-底角=2*atan((底边/2)/高)。 90 - 2 * Math.atan(高/(底边/2))
 -->
 <template>
-  <v-card class="wheel_container"
-          color="primary">
+  <v-card class="wheel_container">
     <div class="wheel_out">
+      <!-- 开始停止按钮 -->
       <v-btn color="primary"
              class="btn_start"
              x-large
              fab
-             @click="start()">
-        开始
+             @click="toogleRandom()">
+        {{ scroll_status ? '停止' : '开始' }}
       </v-btn>
     </div>
     <!-- 奖池外框 + 指针 -->
     <div id="jackport_container"
-         :style="'transform:rotate(' + scrollNums * (360 / items.length) + 'deg)'">
+         :style="'transform:rotate(' + scroll_Nums * (360 / items.length) + 'deg)'">
       <!-- 奖池 -->
       <span v-for="(item, i) in items"
             :key="i"
-            :style="'transform:rotate('+ (i - 1) * (360 / items.length) + 'deg);--borderWdith:'+ ( 360 /items.length) * 2 +'px;'">
+            :style="'transform:rotate('+ i * (360 / items.length) + 'deg);--borderWdith:'+ ( 360 /items.length) * 2 +'px;'">
         <!-- 奖池上显示的文字 -->
         <i>{{ item }}</i>
       </span>
     </div>
+    <h1></h1>
   </v-card>
 </template>
 <script>
+import _ from 'lodash'
 export default {
   name: 'components_randomwheel',
   data () {
     return {
       // 滚动单位数
-      scrollNums: 0,
+      scroll_Nums: 0,
+      scroll_status: false,
+
+      timer_start: null,
+
       // 等腰三角形数量
       items: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
     }
   },
   methods: {
+    shuffle: function () {
+      this.items = _.shuffle(this.items)
+    },
+    toogleRandom () {
+      if (!this.scroll_status) {
+        //! 开始
+        this.start()
+        this.scroll_status = true
+      } else {
+        //! 停止     
+        clearTimeout(this.timer_start)
+        this.timer_start = null
+        this.scroll_status = false
+      }
+    },
+
     start () {
+      //! 如果速度到1 最大速度了 不再加速
+      this.scroll_Nums++
+      this.timer_start = setTimeout(() => {
+        this.start()
+      }, 50)
     },
   },
-  mounted () {
+  computed: {
 
   }
 }
