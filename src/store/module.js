658132667cbbfc5d5581a688bd12f840d7496bf9
黄宇 <huangyu@3xdata.cn>
2021-04-15 21:30:28 +0800

    简化store数据结构

--- src/store/module.js
+++ src/store/module.js
@@ -2,7 +2,7 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2020-12-17 10:12:36
  * @LastEditors: 黄宇/hyuishine
- * @LastEditTime: 2020-12-17 10:13:15
+ * @LastEditTime: 2021-04-01 17:06:22
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
@@ -11,22 +11,11 @@
 const file = require.context("@/view", true, /module.js$/);
 let module = {
     state: {},
-    actions: {},
-    mutations: {},
-    getters: {}
 };
 file.keys().forEach(key => {
     module.state = {
         ...module.state,
         ...(file(key).default || file(key)).state
-    };
-    module.getters = {
-        ...module.getters,
-        ...(file(key).default || file(key)).getters
-    };
-    module.mutations = {
-        ...module.mutations,
-        ...(file(key).default || file(key)).mutations
     };
 });
 
