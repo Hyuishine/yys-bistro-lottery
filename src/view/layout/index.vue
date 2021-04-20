30b24cdf722624adc5db4846ac336e2418a8804e
hyuishine <hyuishine@gmail.com>
2021-04-20 17:38:10 +0800

    去除多余盒模型

--- src/view/layout/index.vue
+++ src/view/layout/index.vue
@@ -9,32 +9,30 @@
  * @youWant: add you want
 -->
 <template>
-  <div>
-    <!-- 主内容 打开菜单时 缩小 通过绑定侧栏菜单状态 拼接字符串匹配样式-->
-    <v-app id="inspire">
-      <v-container fluid>
-        <v-main>
-          <v-flex id="viewBox">
-            <v-layout flex
-                      justify-space-between
-                      row
-                      wrap>
-              <v-row align="center"
-                     justify="center">
-                <v-col :lg="8"
-                       :md="10"
-                       :sm="12"
-                       :xs="12">
-                  <!-- 真的内容，敢于嵌套多层娃 -->
-                  <keep-alive>
-                    <router-view></router-view>
-                  </keep-alive>
-                </v-col>
-              </v-row>
-            </v-layout>
-          </v-flex>
-        </v-main>
-      </v-container>
-    </v-app>
-  </div>
+  <!-- 主内容 打开菜单时 缩小 通过绑定侧栏菜单状态 拼接字符串匹配样式-->
+  <v-app id="inspire">
+    <v-container fluid>
+      <v-main>
+        <v-flex id="viewBox">
+          <v-layout flex
+                    justify-space-between
+                    row
+                    wrap>
+            <v-row align="center"
+                   justify="center">
+              <v-col :lg="8"
+                     :md="10"
+                     :sm="12"
+                     :xs="12">
+                <!-- 真的内容，敢于嵌套多层娃 -->
+                <keep-alive>
+                  <router-view></router-view>
+                </keep-alive>
+              </v-col>
+            </v-row>
+          </v-layout>
+        </v-flex>
+      </v-main>
+    </v-container>
+  </v-app>
 </template>
