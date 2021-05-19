12fc844287cc41dfdaa48af314c8ba17fdbecadf
Hyuishine <hyuishine@gmail.com>
2021-05-19 11:49:43 +0800

    Feat：轮盘，删除本地开始前设置组件。

--- src/view/wheelRandom/index.vue
+++ src/view/wheelRandom/index.vue
@@ -1,19 +1,7 @@
 <template>
   <v-sheet>
-    <weel-random style="margin:100px auto; opacity: .1;"
-                 :options="btnData"
+    <weel-random style="margin:100px auto;"
                  @scrolling_status="scrolling_status"></weel-random>
-    <v-row class="btn_container"
-           type="flex"
-           align="center"
-           justify="center">
-      <!-- 下方操作按钮 传入方法名、文本显示逻辑：有描述(desc)时显示desc,否则判断状态显示 -->
-      <v-btn :loading="btn_loading">选择数据</v-btn>
-      <v-btn v-for="(btn, i) in btnData"
-             :loading="btn_loading"
-             @click="btn_handler(i)"
-             :key="i">{{ btn.desc ? btn.desc : (btn.status ? btn.desc_true : btn.desc_false) }}</v-btn>
-    </v-row>
   </v-sheet>
 </template>
 <script>
@@ -25,11 +13,6 @@ export default {
     return {
       // 按钮点击节流阀、按钮数据(转盘参数)
       btn_loading: false,
-      btnData: [
-        { name: 'mod', desc_true: '随机旋转', desc_false: '手动停止', status: false },
-        { name: 'direction', desc_true: '顺时针旋转', desc_false: '逆时针旋转', status: true },
-        { name: 'reCircle', desc_true: '开启回弹', desc_false: '关闭回弹', status: false },
-      ],
     }
   },
   methods: {
