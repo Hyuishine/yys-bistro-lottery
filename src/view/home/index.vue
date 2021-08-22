c8c6dba5f3a99fce9a989d0f86e8edaa24ae9c03
hyuishine <hyuishine@gmail.com>
2021-08-22 21:54:22 +0800

    提高流动广告权重，放入layout布局视图中。

--- src/view/home/index.vue
+++ src/view/home/index.vue
@@ -2,7 +2,7 @@
  * @Author: 黄宇/Hyuishine
  * @Date: 2021-08-11 22:27:58
  * @LastEditors: 黄宇/Hyuishine
- * @LastEditTime: 2021-08-11 23:14:13
+ * @LastEditTime: 2021-08-22 21:03:32
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
@@ -10,30 +10,25 @@
 -->
 <template>
   <v-row>
-    <!-- 顶部流动广告 -->
-    <v-col cols="12">
-      <v-card>
-        <v-card-title>赞助流动文字广告</v-card-title>
-      </v-card>
-    </v-col>
     <!-- 左侧数据管理 -->
     <v-col cols="6">
       <v-row>
         <v-col cols="5">
           <v-row>
             <v-col cols="12">
-              <v-card>奖池管理</v-card>
+              <v-card hover>奖池管理</v-card>
             </v-col>
             <v-col cols="12">
-              <v-card>人员管理</v-card>
+              <v-card hover>人员管理</v-card>
             </v-col>
             <v-col cols="12">
-              <v-card>赞助管理</v-card>
+              <v-card hover>赞助管理</v-card>
             </v-col>
           </v-row>
         </v-col>
         <v-col cols="7">
-          <v-card>一键导入数据</v-card>
+          <v-card hover
+                  @click="toImport()">一键导入数据</v-card>
         </v-col>
       </v-row>
     </v-col>
@@ -41,25 +36,43 @@
     <v-col cols="6">
       <v-row>
         <v-col cols="7">
-          <v-card>随机出抽奖方式</v-card>
+          <v-card hover>随机出抽奖方式</v-card>
         </v-col>
         <v-col cols="5">
           <v-row>
             <v-col cols="12">
-              <v-card>转盘抽奖</v-card>
+              <v-card hover>转盘抽奖</v-card>
             </v-col>
             <v-col cols="12">
-              <v-card>老虎机滚筒</v-card>
+              <v-card hover>老虎机滚筒</v-card>
             </v-col>
             <v-col cols="12">
-              <v-card>普通格子抽奖</v-card>
+              <v-card hover>普通格子抽奖</v-card>
             </v-col>
           </v-row>
         </v-col>
       </v-row>
     </v-col>
     <v-col cols="12">
-      <v-card>中奖情况统计图</v-card>
+      <v-card hover>中奖情况统计图</v-card>
     </v-col>
   </v-row>
 </template>
+<script>
+export default {
+  name: 'pageHome',
+  mounted () {
+    console.log(this)
+  },
+  methods: {
+    // 跳往导入数据页，
+    toImport () {
+      this.$router.push(
+        {
+          name: 'dataImport'
+        }
+      )
+    }
+  }
+}
+</script>
--- src/view/layout/index.vue
+++ src/view/layout/index.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: 黄宇/hyuishine
  * @Date: 2020-12-09 22:44:42
- * @LastEditors: 黄宇/hyuishine
- * @LastEditTime: 2020-12-13 22:08:44
+ * @LastEditors: 黄宇/Hyuishine
+ * @LastEditTime: 2021-08-22 20:39:31
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
@@ -20,11 +20,21 @@
                     wrap>
             <v-row align="center"
                    justify="center">
+              <!-- 顶部流动广告 -->
               <v-col :lg="8"
                      :md="10"
                      :sm="12"
                      :xs="12">
-                <!-- 真的内容，敢于嵌套多层娃 -->
+                <v-card hover>
+                  <v-card-title>赞助流动文字广告</v-card-title>
+                </v-card>
+              </v-col>
+
+              <!-- 真的内容，敢于嵌套多层娃 -->
+              <v-col :lg="8"
+                     :md="10"
+                     :sm="12"
+                     :xs="12">
                 <keep-alive>
                   <router-view></router-view>
                 </keep-alive>
