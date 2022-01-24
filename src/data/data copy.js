7880a04bb805f8d5925f4b8109dffb95d4b94e62
Hyuishine <hyuishine@gmail.com>
2022-01-24 20:15:16 +0800

    更改数据模板

--- src/data/data copy.js
+++ src/data/data copy.js
@@ -2,11 +2,11 @@
  * @Author: 黄宇/hyuishine
  * @Date: 2022-01-09 14:28:22
  * @LastEditors: 黄宇/Hyuishine
- * @LastEditTime: 2022-01-24 20:12:07
+ * @LastEditTime: 2022-01-18 21:50:46
  * @Description: 
  * @Email: hyuishine@gmail.com
  * @Company: 3xData
- * @youWant: 2022/01/24 敲定数据版本
+ * @youWant: add you want
  */
 
 // 全局数据：奖池、人员、获奖数据
@@ -26,30 +26,17 @@ export default {
         // ],
 
         //! 元数据  需要导入的数据，导入数据的模板 和需要的数据
-        sheetData: {
-
-            peoples: [
-                // {
-                //     name: '人员称呼', // 唯一，必要，通过这个来生成人员id
-                //     howContact: '联系方式',
-                //     giftName: '赞助奖品名称',
-                //     ID:'游戏ID', 
-                //     rank:'斗技分'
-                //     contribution:'勋章数'
-                // }
-            ],
-            gifts: [
-                // {
-                //     name: '人员称呼', // 唯一，必要，通过这个来生成人员id
-                //     howContact: '联系方式',
-                //     giftName: '赞助奖品名称',
-                //     giftAmount: '赞助数量',
-                //     giftRemark: '奖品详情',
-                //     giftID:'奖品ID'
-                // }
-            ]
-
-        },
+        sheetData: [
+            // {
+            //     name: '人员称呼', // 唯一，必要，通过这个来生成人员id
+            //     howContact: '联系方式',
+            //     peopleRemark: '人员备注',
+            //     giftName: '赞助奖品名称',
+            //     giftAmount: '赞助数量',
+            //     giftRemark: '奖品备注/详情',
+            //     peopleID: '人员id', // excel中不需要填写，自动生成
+            // }
+        ],
 
         //! 正在使用的数据
         using: {
@@ -57,48 +44,49 @@ export default {
             // 人员
             peoples: [
                 // {
-                //     name: '人员称呼',
+                //     name: '称呼',
                 //     howContact: '联系方式',
-                //     giftName: '赞助奖品名称',
-                //     ID:'游戏ID',  // 唯一，必要
-                //     rank:'斗技分'
-                //     contribution:'勋章数'
                 //     missTime: '中奖错过次数',
+                //     giftName: '赞助奖品名称',
+                //     giftAmount: '赞助数量',
+                //     peopleRemark: '备注',
+                //     giftRemark: '奖品备注/详情',
+                //     peopleID: '人员id',
                 //     awarded: false // 是否中奖
                 // }
             ],
 
             // 可参与抽奖人员信息
             canRandom: [
-                //     name: '人员称呼',
+                //     name: '称呼',
                 //     howContact: '联系方式',
-                //     giftName: '赞助奖品名称',
-                //     ID:'游戏ID', 
-                //     rank:'斗技分'
-                //     contribution:'勋章数'
                 //     missTime: '中奖错过次数',
+                //     peopleRemark: '备注',
+                //     peopleID: '人员id',
             ],
 
             // 剩余奖品
             lastGifts: [
                 // {
-                //     name: '人员称呼',
-                //     howContact: '联系方式',
+                //     name: '赞助人称呼',
+                //     howContact: '赞助人联系方式',
                 //     giftName: '赞助奖品名称',
-                //     giftAmount: '赞助数量',
-                //     giftRemark: '奖品详情',
-                //     giftID:'奖品ID'
+                //     giftAmount: '提供数量',
+                //     remaining: '剩余数量',
+                //     giftRemark: '备注/详情',
+                //     peopleID: '赞助人id',
             ],
 
             // 奖池
             gifts: [
                 // {
-                //     name: '人员称呼',
-                //     howContact: '联系方式',
+                //     name: '赞助人称呼',
+                //     howContact: '赞助人联系方式',
                 //     giftName: '赞助奖品名称',
-                //     giftAmount: '赞助数量',
-                //     giftRemark: '奖品详情',
-                //     giftID:'奖品ID'
+                //     giftAmount: '提供数量',
+                //     remaining: '剩余数量',
+                //     giftRemark: '备注/详情',
+                //     peopleID: '赞助人id',
                 // }
             ],
 
@@ -108,11 +96,13 @@ export default {
                 //     winner: '中奖人称呼',
                 //     sponsor: '赞助人称呼',
                 //     giftName: '奖品名称',
+                //     winningsNum: '中奖数量',
                 //     winnerContact: '中奖人联系方式',
                 //     sponsorContact: '赞助人联系方式',
-                //     giftRemark: '奖品详情',
-                //     sponsorAD: '赞助人广告',
-                //     winnerID: '中奖人游戏ID',
+                //     giftRemark: '奖品备注/详情',
+                //     sponsorRemarks: '赞助人备注',
+                //     winnerID: '中奖人id',
+                //     sponsorID: '赞助人id',
                 // }
             ],
         },
